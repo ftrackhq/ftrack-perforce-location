3a77083a29a3a9ade4a36dc25f11c6a0ee4fb823
l_angeli <lorenzo.angeli@ftrack.com>
2019-09-11 11:18:41 +0200

    move modules in a system named folder

--- source/ftrack_perforce_location/import_p4api.py
+++ source/ftrack_perforce_location/import_p4api.py
@@ -4,12 +4,21 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+os_mapping = {
+    'win32': 'windows',
+    'darwin': 'osx',
+    'linux2': 'linux'
+}
+
+
 def import_p4():
     cwd = os.path.dirname(__file__)
-    p4_modules = os.path.abspath(os.path.join(cwd, '..', '..', 'modules'))
+    p4_modules = os.path.abspath(os.path.join(cwd, '..', '..', 'modules', os_mapping[sys.platform]))
     modules = os.listdir(p4_modules)
     for module in modules:
+
         full_path = os.path.join(p4_modules, module)
+        logger.debug('looking for module in path: {}'.format(full_path))
         if full_path not in sys.path:
             sys.path.append(full_path)
         try:
@@ -20,5 +29,5 @@ def import_p4():
             if sys.path[-1] == full_path:
                 del sys.path[-1]
         else:
-            logger.warning('SUCCESSFULLY LOADED {}'.format(full_path))
+            logger.debug('successfully loaded {}'.format(full_path))
             break
