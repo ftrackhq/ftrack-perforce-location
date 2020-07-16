bdefe4f9a445f64bd47934b2d3b531c7f762d3be
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-07-16 14:41:24 +0200

    improve and consolidate debut on post publish hook

--- resource/location/post_publish_hook.py
+++ resource/location/post_publish_hook.py
@@ -173,7 +173,7 @@ def _register(event, session=None):
         return
 
     location_id = location['id']
-    logger.debug('registering post publish hook for location {}'.format(
+    logger.debug('Registering post publish hook for location {}'.format(
         SCENARIO_ID)
     )
 
@@ -193,6 +193,8 @@ def register(api_object, **kw):
     if not isinstance(api_object, ftrack_api.Session):
         # Exit to avoid registering this plugin again.
         return
+
+    logger.info('Discovering post publish hook')
 
     api_object.event_hub.subscribe(
         'topic=ftrack.api.session.ready',
