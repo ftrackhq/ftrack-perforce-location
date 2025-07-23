2ba10925c281bd73884651232a2efd1204d7fca0
lorenzo angeli <lorenzo.angeli@backlight.co>
2025-07-23 14:12:20 -0700

    restore old logic for storage scenario config

--- source/ftrack_perforce_location/scenario.py
+++ source/ftrack_perforce_location/scenario.py
@@ -35,18 +35,20 @@ class ConfigurePerforceStorageScenario(object):
     @property
     def storage_scenario(self):
         '''Return storage scenario setting.'''
+        return self.session.query(
+            'select value from Setting '
+            'where name is "storage_scenario" and group is "STORAGE"'
+        ).one()        
 
-        if 'storage_scenario' not in self.session.server_information:
-            return None
+        # if 'storage_scenario' not in self.session.server_information:
+        #     return None
         
-        storage_scenario = self.session.server_information.get(
-            'storage_scenario'
-        )
-        data = storage_scenario['data']
+        # storage_scenario = self.session.server_information.get(
+        #     'storage_scenario'
+        # )
+        # data = storage_scenario['data']
         # if data.keys() != PERFORCE_STORAGE_REQUIRED_FIELDS:
         #     raise PerforceValidationError('Perforce scenario fields do not match.')
-
-        return data
 
     @property
     def existing_perforce_storage_configuration(self):
